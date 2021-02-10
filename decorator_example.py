d6d43cd5a01617a969329af5e2d3694442527a2d
paul <parndtls1biz@gmail.com>
2021-02-10 13:43:48 -0800

    new file

---
+++ decorator_example.py
@@ -0,0 +1,56 @@
+def counter(fn):
+    count = 0
+	
+    def inner(*args, **kwargs):
+        nonlocal count
+        count += 1
+        print(f'func {fn} was called {count} times')
+        return fn(*args, **kwargs)
+		
+    return inner
+	
+def add(a:int, b:int=0):
+    '''add two integers'''
+    return a + b
+	
+id(add)	# def add function
+add = counter(add)
+id(add) # new inner function
+print(add(10, 20))
+print(add(10))
+print(add(20,40))
+
+def mult(a: int, b: int=1):
+    '''multiply two integers'''
+    return a * b
+	
+mult = counter(mult)
+print(mult(1, 2))
+print(mult(1))
+print(mult(3, 8))
+
+# --or-- user decorator
+@counter
+def add(a:int, b:int=0):
+    '''add two integers'''
+    return a + b
+
+print(add(10, 20))
+print(add(10))
+print(add(20,40))
+
+@counter
+def mult(a: int, b: int=1):
+    '''multiply two integers'''
+    return a * b
+	
+print(mult(1, 2))
+print(mult(1))
+print(mult(3, 8))
+
+	
+
+
+
+
+	
